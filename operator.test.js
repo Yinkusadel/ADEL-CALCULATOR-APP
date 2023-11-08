5438b238a3c78f242e692f3347a555c3df46dac3
yinkusadel <adeleyeolayinka73@gmail.com>
2023-11-08 13:49:21 +0100

    Feat: update tests

--- operator.test.js
+++ operator.test.js
@@ -2,7 +2,16 @@ const operate = require('./operator');
 
 test('adds 1 + 2 to equal 3', () => {
   expect(operate('+', 1, 2)).toBe(3);
+});
+
+test('subtract 2 - 1 to equal 1', () => {
   expect(operate('-', 2, 1)).toBe(1);
+});
+
+test('multiply 1 * 2 to equal 2', () => {
   expect(operate('*', 1, 2)).toBe(2);
+});
+
+test('subtract 4 / 2 to equal 2', () => {
   expect(operate('/', 4, 2)).toBe(2);
 });
