ced90dbd873b02d1a7e4f2a344996d625a0cf40c
yinkusadel <adeleyeolayinka73@gmail.com>
2023-11-08 01:07:32 +0100

    Feat:add operator function and jest config flle

---
+++ operator.js
@@ -0,0 +1,20 @@
+const operate = (operator, num1, num2) => {
+  switch (operator) {
+    case '+':
+      return num1 + num2;
+    case '-':
+      return num1 - num2;
+    case '*':
+      return num1 * num2;
+    case '/':
+      if (num2 !== 0) {
+        return num1 / num2;
+      }
+      return 'Division by zero is not allowed';
+
+    default:
+      return 'pick operator';
+  }
+};
+
+operate();
