efce05a3e436bc18a4aafcc80535ddc22e300a3a
yinkusadel <adeleyeolayinka73@gmail.com>
2023-11-08 02:22:25 +0100

    Feat:add tests for other operators

--- operator.test.js
+++ operator.test.js
@@ -2,4 +2,7 @@ const operate = require('./operator');
 
 test('adds 1 + 2 to equal 3', () => {
   expect(operate('+', 1, 2)).toBe(3);
+  expect(operate('-', 2, 1)).toBe(1);
+  expect(operate('*', 1, 2)).toBe(2);
+  expect(operate('/', 4, 2)).toBe(2);
 });
